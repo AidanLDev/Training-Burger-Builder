6129dd4d9e7c8403f6c271331c44402b46459aa3
Aidan <aidan.lowson@contactpartners.com>
2019-04-11 13:29:33 +0100

    New HOC takes in className to style wrapped code

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
+import WithClass from '../../hoc/WithClass'
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
 import styles from './burger.css'
 
 const burger = (props) => {
     return (
-        <div className={styles.burger}>
+        // <div className={styles.burger}>
+        <WithClass class={styles.burger}>
             <BurgerIngredients type="bread-top" />
             <BurgerIngredients type="meat" />
             <BurgerIngredients type="bread-bottom" />
-        </div>
+            </WithClass>
+        // </div>
     );
 };
 
