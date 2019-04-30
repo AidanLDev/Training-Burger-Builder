e186b1ba7958e2f2037fe230006e0a07e56271a1
Aidan Lowson <aidan.lowson@contactpartners.com>
2019-04-30 17:25:37 +0100

    small style

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -10,7 +10,7 @@ const checkoutSummary = (props) => {
     return (
         <div className={styles.checkoutSummary}>
             <h1>Hope it's tasty</h1>
-            <div style={{width: '100%',  margin: 'auto'}}>
+            <div style={{width: '100%', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients}/>
             </div>
         <Button btnType="danger" clicked>CANCEL</Button>
