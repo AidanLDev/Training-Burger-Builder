6a120dbae265b2f60440760d856d5832abe4686d
Aidan <aidanlowson@gmail.com>
2019-07-07 20:54:01 +0100

    Getting rid of lazy loading for now and extra overflow

--- src/App.js
+++ src/App.js
@@ -1,74 +1,60 @@
-import React, { useEffect, Suspense } from 'react';
-import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react'
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-import Layout from './hoc/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Logout from './containers/Auth/Logout/Logout';
-import * as actions from './store/actions/index';
-
-const Orders = React.lazy(() => {
-  return require('./containers/Orders/Orders');
-});
-
-const Checkout = React.lazy(() => {
-  return require('./containers/Checkout/Checkout');
-});
-
-const Auth = React.lazy(() => {
-  return require('./containers/Auth/Auth');
-});
+import Layout from './hoc/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import Checkout from './containers/Checkout/Checkout'
+import Orders from './containers/Orders/Orders'
+import Auth from './containers/Auth/Auth'
+import Logout from './containers/Auth/Logout/Logout'
+import * as actions from './store/actions/index'
 
 const app = props => {
+
   useEffect(() => {
     props.onTryAutoSignup();
-  }, []);
+  }, [])
 
-  let routes = (
-    <Switch>
-      <Route path="/auth" render={() => <Auth />} />
-      <Route path="/" exact component={BurgerBuilder} />
-      <Redirect to="/" />
-    </Switch>
-  );
-
-  if (props.isAuthenticated) {
-    routes = (
+    let routes = (
       <Switch>
-        <Route path="/checkout" render={() => <Checkout />} />
-        <Route path="/orders" render={() => <Orders />} />
-        <Route path="/logout" component={Logout} />
-        <Route path="/auth" render={() => <Auth />} />
         <Route path="/" exact component={BurgerBuilder} />
+        <Route path="/auth" component={Auth} />
         <Redirect to="/" />
       </Switch>
     );
-  }
 
-  return (
-    <div>
-      <Layout>
-        <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
-      </Layout>
-    </div>
-  );
-};
+    if (props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/logout" component={Logout} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+    return (
+      <div>
+        <Layout>
+          {routes}
+        </Layout>
+      </div>
+    );
+  }
 
 const mapStateToProps = state => {
   return {
     isAuthenticated: state.auth.token !== null
-  };
-};
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
     onTryAutoSignup: () => dispatch(actions.authCheckState())
-  };
-};
+  }
+}
 
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(app)
-);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(app));
