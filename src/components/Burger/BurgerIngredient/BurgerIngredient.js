e9a01a72b4886fa28485177b0dfa5d4fa292c5ca
Aidan Lowson <aidan.lowson@contactpartners.com>
2019-07-10 11:34:53 +0100

    Convertions

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import classes from './BurgerIngredient.css';
 
-const burgerIngredient {
+const burgerIngredient = props => {
     let ingredient = null;
 
-    switch ( this.props.type ) {
+    switch ( props.type ) {
         case ( 'bread-bottom' ):
             ingredient = <div className={classes.BreadBottom}></div>;
             break;
@@ -38,8 +38,8 @@ const burgerIngredient {
 
 }
 
-BurgerIngredient.propTypes = {
+burgerIngredient.propTypes = {
     type: PropTypes.string.isRequired
 };
 
-export default BurgerIngredient;
+export default burgerIngredient;
