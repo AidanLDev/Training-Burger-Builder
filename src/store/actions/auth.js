33e42d75a90ecb833bf449fef361fb79bd4502a0
Aidan <aidan.lowson@contactpartners.com>
2020-05-24 21:43:26 +0100

    Dispatch auth error

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,104 +1,119 @@
 import axios from 'axios';
 
 import * as actionTypes from './actionTypes';
-import { displayToast } from './utils'
+import { displayToast } from './utils';
 
 export const authStart = () => {
-    return {
-        type: actionTypes.AUTH_START
-    };
+  return {
+    type: actionTypes.AUTH_START,
+  };
 };
 
 export const authSuccess = (token, userId) => {
-    return {
-        type: actionTypes.AUTH_SUCCESS,
-        idToken: token,
-        userId: userId
-    };
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    idToken: token,
+    userId: userId,
+  };
 };
 
 export const authSuccessToast = (userName, isSignup) => {
-    const isSignUpMessage = 'Welcome';
-    const signInMessage = 'Welcome back'
-    return dispatch => {
-        dispatch(displayToast(`${isSignup ? isSignUpMessage : signInMessage} ${userName}`, 'SUCCESS', true))
-    }
-}
+  const isSignUpMessage = 'Welcome';
+  const signInMessage = 'Welcome back';
+  return (dispatch) => {
+    dispatch(
+      displayToast(
+        `${isSignup ? isSignUpMessage : signInMessage} ${userName}`,
+        'SUCCESS',
+        true
+      )
+    );
+  };
+};
 
 export const authFail = (error) => {
-    return {
-        type: actionTypes.AUTH_FAIL,
-        error: error
-    };
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error,
+  };
 };
 
 export const logout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('expirationDate');
-    localStorage.removeItem('userId');
-    return {
-        type: actionTypes.AUTH_LOGOUT
-    };
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationDate');
+  localStorage.removeItem('userId');
+  return {
+    type: actionTypes.AUTH_LOGOUT,
+  };
 };
 
 export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
-        setTimeout(() => {
-            dispatch(logout());
-        }, expirationTime * 1000);
-    };
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
 };
 
 export const auth = (email, password, isSignup) => {
-    return dispatch => {
-        dispatch(authStart());
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCDLNQBbrdXQIg1xcXVg1xqx9xvJVhNM5s';
-        if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCDLNQBbrdXQIg1xcXVg1xqx9xvJVhNM5s';
-        }
-        axios.post(url, authData)
-            .then(response => {
-                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
-                localStorage.setItem('token', response.data.idToken);
-                localStorage.setItem('expirationDate', expirationDate);
-                localStorage.setItem('userId', response.data.localId);
-                dispatch(authSuccess(response.data.idToken, response.data.localId));
-                dispatch(checkAuthTimeout(response.data.expiresIn));
-                dispatch(authSuccessToast(email, isSignup))
-            })
-            .catch(err => {
-                // dispatch(authFail(err.response.data.error));
-                console.log('(auth action)', err)
-            });
+  return (dispatch) => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
     };
+    let url =
+      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCDLNQBbrdXQIg1xcXVg1xqx9xvJVhNM5s';
+    if (!isSignup) {
+      url =
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCDLNQBbrdXQIg1xcXVg1xqx9xvJVhNM5s';
+    }
+    axios
+      .post(url, authData)
+      .then((response) => {
+        const expirationDate = new Date(
+          new Date().getTime() + response.data.expiresIn * 1000
+        );
+        localStorage.setItem('token', response.data.idToken);
+        localStorage.setItem('expirationDate', expirationDate);
+        localStorage.setItem('userId', response.data.localId);
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
+        dispatch(authSuccessToast(email, isSignup));
+      })
+      .catch((err) => {
+        dispatch(authFail(err.response.data.error));
+        console.log('(auth action)', err);
+      });
+  };
 };
 
 export const setAuthRedirectPath = (path) => {
-    return {
-        type: actionTypes.SET_AUTH_REDIRECT_PATH,
-        path: path
-    };
+  return {
+    type: actionTypes.SET_AUTH_REDIRECT_PATH,
+    path: path,
+  };
 };
 
 export const authCheckState = () => {
-    return dispatch => {
-        const token = localStorage.getItem('token');
-        if (!token) {
-            dispatch(logout());
-        } else {
-            const expirationDate = new Date(localStorage.getItem('expirationDate'));
-            if (expirationDate <= new Date()) {
-                dispatch(logout());
-            } else {
-                const userId = localStorage.getItem('userId');
-                dispatch(authSuccess(token, userId));
-                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000 ));
-            }   
-        }
-    };
+  return (dispatch) => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+      if (expirationDate <= new Date()) {
+        dispatch(logout());
+      } else {
+        const userId = localStorage.getItem('userId');
+        dispatch(authSuccess(token, userId));
+        dispatch(
+          checkAuthTimeout(
+            (expirationDate.getTime() - new Date().getTime()) / 1000
+          )
+        );
+      }
+    }
+  };
 };
